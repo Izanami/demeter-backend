1a93ec607d32eb02ec453da8e548e7ee9e2b53c8
Adrien Jeser <adrien@jeser.me>
2016-08-02 19:08:32 +0200

    Sensor

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Sensor.create(name: 'Temperature', value: 20.0)
